cda3b400925c4366e4e783e120a1ed41cbe09c87
Ubuntu <ubuntu@ip-172-31-37-57.ec2.internal>
2015-04-10 05:12:43 +0000

    enable functionality to query api

--- app/controllers/merger_controller.rb
+++ app/controllers/merger_controller.rb
@@ -1,4 +1,6 @@
 class MergerController < ApplicationController
+  require 'net/http'
+
   API_URL = 'https://api.pelotoncycle.com/quiz/next/'
 
   def merge_streams
@@ -7,16 +9,39 @@ class MergerController < ApplicationController
     stream1 = params[:stream1]
     stream2 = params[:stream2]
     
+    # ensure we have necessary params
     unless stream1 and stream2
       render :json => ret
       return
     end
 
+    # get responses from api
+    response1 = get_api_response stream1
+    response2 = get_api_response stream2
+
+    
     render :json => { 
       :status => :ok, 
       :message => "Success!",
       :html => "<b>congrats</b>",
-      :params => params
+      :params => params,
+      :response1 => response1,
+      :response2 => response2
     }.to_json
+  end
+
+  private
+  
+  def get_api_response stream_name
+    uri = URI.parse(API_URL + stream_name)
+    #args = {include_entities: 0, include_rts: 0, screen_name: 'johndoe', count: 2, trim_user: 1}
+    #uri.query = URI.encode_www_form(args)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    
+    request = Net::HTTP::Get.new(uri.request_uri)
+    
+    response = http.request(request)
+    JSON.parse response.body
   end
 end
